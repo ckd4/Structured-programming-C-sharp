83ca837c2dacb9b5d51c64f776baac8a071528ee
ckd4 <107938367+ckd4@users.noreply.github.com>
2023-01-16 15:57:24 +0300

    Add files via upload

---
+++ WFA4/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WFA4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbToCompare1 = new System.Windows.Forms.TextBox();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.tbToCompare2 = new System.Windows.Forms.TextBox();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbToCompare1
+            // 
+            this.tbToCompare1.Location = new System.Drawing.Point(12, 120);
+            this.tbToCompare1.Name = "tbToCompare1";
+            this.tbToCompare1.Size = new System.Drawing.Size(189, 20);
+            this.tbToCompare1.TabIndex = 0;
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(169, 173);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(90, 34);
+            this.btnCompare.TabIndex = 1;
+            this.btnCompare.Text = "Сравнить";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.btnCompare_Click);
+            // 
+            // tbToCompare2
+            // 
+            this.tbToCompare2.Location = new System.Drawing.Point(245, 120);
+            this.tbToCompare2.Name = "tbToCompare2";
+            this.tbToCompare2.Size = new System.Drawing.Size(196, 20);
+            this.tbToCompare2.TabIndex = 2;
+            // 
+            // lblOut
+            // 
+            this.lblOut.AutoSize = true;
+            this.lblOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblOut.Location = new System.Drawing.Point(49, 52);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(0, 25);
+            this.lblOut.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 295);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.tbToCompare2);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.tbToCompare1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbToCompare1;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.TextBox tbToCompare2;
+        private System.Windows.Forms.Label lblOut;
+    }
+}
+
---
+++ WFA4/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            StringBuilder resultString1 = new StringBuilder();
+            StringBuilder resultString2 = new StringBuilder();
+
+            if (tbToCompare1.Text.Length < 15) /*Если первая строка короче 15 символов, 
+                                                то в строку записывается каждый второй символ, начиная с чётного*/
+            {
+                for (int i = 0; i < tbToCompare2.Text.Length; i = i + 2)
+                    resultString1.Append("" + tbToCompare2.Text[i]);
+                lblOut.Text = resultString1.ToString(); //Итоговая строка записывается в метку
+            }
+            else //Иначе сохраняется исходная строка
+                resultString1.Append(tbToCompare1.Text);
+
+            if (tbToCompare2.Text.Length > 30) /*Если вторая строка длиннее 30 символов,
+                                               то в строку записывается каждый второй символ, начиная с нечётного*/
+            {
+                for (int i = 1; i < tbToCompare1.Text.Length; i = i + 2)
+                    resultString2.Append("" + tbToCompare1.Text[i]);
+                lblOut.Text = resultString2.ToString(); //Итоговая строка записывается в метку
+            }
+            else
+                resultString2.Append(tbToCompare2.Text);
+        }
+    }
+}
